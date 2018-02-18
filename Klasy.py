75a5c2e42fb612e6ef1c2982926e88d3a82496ed
ASzczepaniak <a.szczepaniak333@gmail.com>
2018-02-18 14:05:52 +0100

    Podstawa klas
    No to mamy podstawe klas

--- Klasy.py
+++ Klasy.py
@@ -1,1 +1,13 @@
-print "HELLO WORLD"
+class Auto:
+	color = "red"
+	name = ""
+	price = 20000
+
+	def Description(self):
+		print "My car {} is {} for {}".format(self.name, self.color, self.price)
+
+car1 = Auto()
+
+car1.name = "Ziutek"
+
+car1.Description()
